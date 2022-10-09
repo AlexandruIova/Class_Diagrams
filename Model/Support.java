40c9d55a578392ec6701767d224fd7b394f5a610
AlexIova <iovaalexandru37@gmail.com>
2022-10-09 19:56:15 +0200

    Initial commit

---
+++ Model/Support.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Support {
+
+    /**
+     * Default constructor
+     */
+    public Support() {
+    }
+
+    /**
+     * 
+     */
+    public void role: ERole;
+
+    /**
+     * 
+     */
+    public float hours;
+
+}
---
+++ Model/UniversityMember.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public abstract class UniversityMember extends Class1 {
+
+    /**
+     * Default constructor
+     */
+    public UniversityMember() {
+    }
+
+    /**
+     * 
+     */
+    public String firstName;
+
+    /**
+     * 
+     */
+    public String lastName;
+
+    /**
+     * 
+     */
+    public int ssNo;
+
+}
